e9d3f63bca8c02a2774a8fb0e240bc3785cf137d
Dylan C <dcrawshaw@fender.com>
2025-04-16 17:54:46 -0700

    update

--- next.config.js
+++ next.config.js
@@ -12,8 +12,6 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
-      bufferutil: false,
-      "utf-8-validate": false,
     };
     return config;
   },
